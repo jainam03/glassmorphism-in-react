f4178f7e09896b6a88655fdcd27f93bebb2d7778
jainam03 <jbchheda03@gmail.com>
2022-08-07 20:39:08 +0530

    created some content for the card

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tilt from "react-vanilla-tilt"
+
+import "./Main.css"
+
+export default function Main() {
+    return (
+        <div className="container">
+            <div className="card">
+                <h2>This card will show the effect.</h2>
+
+                <h2>Tilt effect coming soon!</h2>
+
+                <button className="button">Button</button>
+            </div>
+        </div>
+
+
+
+    )
+}
