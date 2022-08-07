ce6f08d710120d22e1179fdefbc2692a47c2c545
jainam03 <jbchheda03@gmail.com>
2022-08-07 20:42:06 +0530

    completed footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h4>©️ Hackerman Development. All rights reserved.</h4>
+        </div>
+    )
+}
