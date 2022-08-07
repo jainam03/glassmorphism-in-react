c0168bb086f424fd6d39b8ba7495b4adcbd43c95
jainam03 <jbchheda03@gmail.com>
2022-08-07 20:20:45 +0530

    completed header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1>
+                <i>React project on Glassmorphism</i>
+            </h1>
+        </div>
+    )
+}
